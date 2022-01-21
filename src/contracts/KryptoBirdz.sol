12de518ef851e4866c9266f115116d038631dc24
[Thisara Hettikankanama] <[thisarayashodagcc@gmail.com]>
2022-01-21 21:57:20 +0530

    inti project

---
+++ src/contracts/KryptoBirdz.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract KryptoBirdz{
+
+    string private name;
+    string public symbol;
+
+    constructor(){
+        name="KryptoBirdz";
+        symbol="Kbirdz";   
+    }
+}

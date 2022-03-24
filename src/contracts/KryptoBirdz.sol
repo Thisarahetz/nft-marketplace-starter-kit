88c2e35701b2e834b152e57fd16ef886b29864f4
[Thisara Hettikankanama] <[thisarayashodagcc@gmail.com]>
2022-03-24 23:19:17 +0530

    REC721

--- src/contracts/KryptoBirdz.sol
+++ src/contracts/KryptoBirdz.sol
@@ -1,13 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract KryptoBirdz{
+import './ERC721Connector.sol';
 
-    string private name;
-    string public symbol;
+contract KryptoBird is ERC721Connector {
 
-    constructor(){
-        name="KryptoBirdz";
-        symbol="Kbirdz";   
+    // array to store our nfts
+    string [] public kryptoBirdz;
+
+    mapping(string => bool) _kryptoBirdzExists;
+
+    function mint(string memory _kryptoBird) public {
+
+        require(!_kryptoBirdzExists[_kryptoBird],
+        'Error - kryptoBird already exists');
+        // this is deprecated - uint _id = KryptoBirdz.push(_kryptoBird);
+        kryptoBirdz.push(_kryptoBird);
+        uint _id = kryptoBirdz.length - 1;
+
+        // .push no longer returns the length but a ref to the added element
+        _mint(msg.sender, _id);
+
+        _kryptoBirdzExists[_kryptoBird] = true;
+
     }
+
+    constructor() ERC721Connector('KryptoBird','KBIRDZ')
+{}
+
 }
+
+
